64a242d494ccde76c21d7de5696dbe745966950d
Chaplin <67743720+leechanggi@users.noreply.github.com>
2023-07-10 21:11:48 +0900

    add router

--- src/components/common/Container.tsx
+++ src/components/common/Container.tsx
@@ -1,13 +1,30 @@
-import React from 'react';
-import TodoContextProvider from '../../context/TodoContextProvider';
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import TodoContextProvider from "../../context/TodoContextProvider";
 
-import { Box } from '../util/Box';
+import { Box } from "../util/Box";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <p>HOME</p>,
+    errorElement: <p>Not Found</p>,
+  },
+  {
+    path: "/todo",
+    element: (
+      <TodoContextProvider>
+        <Box />
+      </TodoContextProvider>
+    ),
+  },
+]);
 
 export default function Container() {
   return (
     <div id="container">
       <TodoContextProvider>
-        <Box />
+        <RouterProvider router={router} />
       </TodoContextProvider>
     </div>
   );
