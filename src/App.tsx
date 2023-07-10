a78abc959278c661caa725dcacdcbcfdeafc42f1
leechanggi <67743720+leechanggi@users.noreply.github.com>
2023-07-10 23:07:10 +0900

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
-import React from "react";
-import Container from "./components/common/Container";
-import ThemeContextProvider from "./context/ThemeContextProvider";
-import "./style/App.scss";
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ThemeContextProvider from './context/ThemeContextProvider';
+import TodoContextProvider from './context/TodoContextProvider';
+import { Box } from './components/util/Box';
+import './style/App.scss';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>HOME</div>,
+    errorElement: <div>NOT FOUND</div>,
+  },
+  {
+    path: '/todo',
+    element: (
+      <TodoContextProvider>
+        <Box />
+      </TodoContextProvider>
+    ),
+  },
+]);
 
 function App() {
   return (
     <ThemeContextProvider>
-      <div className="App">
-        <Container />
-      </div>
+      <RouterProvider router={router} />
     </ThemeContextProvider>
   );
 }
